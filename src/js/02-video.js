10637e19608558d213899ecad0cb6bc370875406
anastasia7makar <107794485+anastasia7makar@users.noreply.github.com>
2023-06-24 01:44:13 +0300

    Added code for the task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const key = 'videoplayer-current-time';
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.on(
+  'timeupdate',
+  throttle(function (currentTime) {
+    window.localStorage.setItem(key, currentTime.seconds);
+  }, 1000)
+);
+
+player.setCurrentTime(window.localStorage.getItem(key) ?? 0);
 
