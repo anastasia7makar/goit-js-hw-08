2e5f09230d2c31197a43cbdf46562261e8468fec
anastasia7makar <107794485+anastasia7makar@users.noreply.github.com>
2023-06-24 18:43:16 +0300

    Added code for the task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,32 @@
-const form = document.querySelector('.feedback-form');
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const KEY_FEEDBACK = 'feedback-form-state';
+
+form.addEventListener('input', throttle(handleInput, 500));
+form.addEventListener('submit', handlerSubmit);
+
+const localProperty =
+  JSON.parse(window.localStorage.getItem(KEY_FEEDBACK)) ?? null;
+
+if (localProperty) {
+  for (const key of Object.keys(localProperty)) {
+    form.elements[key].value = localProperty[key];
+  }
+}
+
+function getData() {
+  const formData = new FormData(form);
+  return Object.fromEntries(formData);
+}
+
+function handleInput(e) {
+  window.localStorage.setItem(KEY_FEEDBACK, JSON.stringify(getData()));
+}
+
+function handlerSubmit(e) {
+  e.preventDefault();
+  console.log(getData());
+  window.localStorage.removeItem(KEY_FEEDBACK);
+  form.reset();
+}
